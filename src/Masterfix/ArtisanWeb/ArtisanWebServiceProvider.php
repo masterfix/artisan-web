c3fb1fc068523f9b63d610e516bf420758dbf901
Patrik Obrist <padx@gmx.net>
2013-06-13 11:18:10 +0200

    initial upload

---
+++ src/Masterfix/ArtisanWeb/ArtisanWebServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Masterfix\ArtisanWeb;
+
+use Illuminate\Support\ServiceProvider;
+
+class ArtisanWebServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('masterfix/artisan-web');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
